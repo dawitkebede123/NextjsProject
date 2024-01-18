ce52122c92572b225922545914e9d4d4b5867536
dawit woldu <60181219+dawitkebede123@users.noreply.github.com>
2024-01-18 13:38:59 -0800

    Contact.jsx

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -38,7 +38,7 @@ const Contact = () => {
                 <p className='uppercase pt-8'>Connect With Me</p>
                 <div className='flex items-center justify-between py-4'>
                   <a
-                    href=https://www.linkedin.com/in/dawit-woldu-063b1b155/'
+                    href='https://www.linkedin.com/in/dawit-woldu-063b1b155/'
                     target='_blank'
                     rel='noreferrer'
                   >
